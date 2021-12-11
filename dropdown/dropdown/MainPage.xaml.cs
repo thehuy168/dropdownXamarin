0357c1b744cefbd116d2022855ed6390af4d5e7d
huy the <thehuy168@hotmail.com>
2021-12-11 11:03:19 +0700

    Add project files.

---
+++ dropdown/dropdown/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace dropdown
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            var countries = GetCountry();
+            pickerC.ItemsSource = countries;
+          
+        }
+        private List<string>GetCountry()
+        {
+            return new List<string>
+            {
+                "Vietnam",
+                "UK",
+                "USA",
+                "Russian"
+                
+            };
+        }
+    }
+}
